40608e7e9dafea6d5ac3c02ce896cf6a01fef8ec
Harsh Aggarwal <harshaggarwal@Harshs-MacBook-Pro.local>
2019-05-02 18:21:38 -0500

    Added make new user page

--- make_new_user.php
+++ make_new_user.php
@@ -6,7 +6,7 @@
     <meta name="author" content="">
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
 
-    <title>Donate for Animals!</title>
+    <title>Add a user!</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -96,7 +96,7 @@ if ($result->num_rows > 0) {
           <div class="col-md-12">
             <div class="section-heading">
               <div class="line-dec"></div>
-              <h1>Donate for an Animal</h1>
+              <h1>Add a new user</h1>
             </div>
           </div>
           
@@ -108,17 +108,37 @@ if ($result->num_rows > 0) {
                   <div class="row">
                     <div class="col-md-6">
                       <fieldset>
-                        <input name="userID" type="integer" class="form-control" id="userID" placeholder="Your user ID..." required="">
+                        <input name="fName" type="text" class="form-control" id="fName" placeholder="Your first name..." required="">
                       </fieldset>
                     </div>
                     <div class="col-md-6">
                       <fieldset>
-                        <input name="animalID" type="integer" class="form-control" id="animalID" placeholder="Animal ID to donate for.." required="">
+                        <input name="lName" type="text" class="form-control" id="lName" placeholder="Your last name..." required="">
                       </fieldset>
                     </div>
                     <div class="col-md-12">
                       <fieldset>
-                        <input name="amount" type="float" class="form-control" id="amount" placeholder="Donation amount.." required="">
+                        <input name="pNumber" type="text" class="form-control" id="pNumber" placeholder="Phone Number.." required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <input name="eMail" type="text" class="form-control" id="eMail" placeholder="Your email address..." required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-md-6">
+                      <fieldset>
+                        <input name="userName" type="text" class="form-control" id="userName" placeholder="Username..." required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-md-6">
+                      <fieldset>
+                        <input name="passWord" type="text" class="form-control" id="passWord" placeholder="Password..." required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-md-6">
+                      <fieldset>
+                        <input name="homeType" type="text" class="form-control" id="homeType" placeholder="Home Type..." required="">
                       </fieldset>
                     </div>
                     <!--
@@ -129,7 +149,7 @@ if ($result->num_rows > 0) {
                     </div> -->
                     <div class="col-md-12">
                       <fieldset>
-                        <button type="submit" id="form-submit" class="button">Donate now!</button>
+                        <button type="submit" id="submit" class="button">CREATE USER!</button>
                       </fieldset>
                     </div>
                     <div class="col-md-12">
@@ -147,15 +167,24 @@ if ($result->num_rows > 0) {
     </div>
 
 
-<?php $user_ID = $_POST["userID"]; ?>
-<?php $animal_ID = $_POST["animalID"]; ?>
-<?php $_amount = $_POST["amount"]; ?>
+<?php $fName = $_POST["fName"]; ?>
+<?php $lName = $_POST["lName"]; ?>
+<?php $pNumber = $_POST["pNumber"]; ?>
+<?php $eMail = $_POST["eMail"]; ?>
+<?php $userName = $_POST["userName"]; ?>
+<?php $passWord = $_POST["passWord"]; ?>
+<?php $homeType = $_POST["homeType"]; ?>
 <?php $d=strtotime("today"); 
 $_date = date("Y-m-d h:i:s", $d); ?>
+<!--INSERT INTO `PROFILE` (`Profile_ID`, `First_Name`, `Last_Name`, `Profile_type`, `Mobile_Number`, `Email`, `Username`, `Password`, `Join_Date`) VALUES
+(1, 'Harsh', 'Aggarwal', 'Guest', '999-999-9999', 'harsh.aggarwal@mavs.uta.edu', 'harsh8', 'password', '2019-04-29 00:00:00');
+-->
+<?php $sql = "Insert into PROFILE (First_Name, Last_Name, Profile_type, Mobile_Number, Email, Username, Password, Join_Date) VALUES ('$fName','$lName','Guest','$pNumber','$eMail','$userName','$passWord','$_date');";
 
-<?php $sql = "Insert into DONATION VALUES('$user_ID','$_date','$_amount','$animal_ID')";
-$result = $conn->query($sql);
-mysqli_query($result);
+  $result = $conn->query($sql);
+  $sql = "Insert into GUEST VALUES (@@IDENTITY,'$homeType')";
+  $result = $conn->query($sql);
+  mysqli_query($result);
 ?>
 
         <!-- Footer Starts Here -->
