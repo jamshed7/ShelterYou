89d1681f5be3f3a901c3d69d41fa60e4834f6147
Jamshed Jahangir <jjdx11@gmail.com>
2019-05-03 01:24:41 -0500

    Changed add_animal.php

--- add_animal.php
+++ add_animal.php
@@ -17,7 +17,10 @@
     <link rel="stylesheet" href="assets/css/tooplate-main.css">
     <link rel="stylesheet" href="assets/css/owl.css">
     <link rel="stylesheet" href="assets/css/flex-slider.css">
-
+<!--
+Tooplate 2114 Pixie
+https://www.tooplate.com/view/2114-pixie
+-->
   </head>
   <?php
 $servername = "localhost";
@@ -30,16 +33,27 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+/*
+$sql = "SELECT Name FROM ANIMAL";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Name is: " . $row["Name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}*/
 ?>
 
   <body>
-
+    
     <!-- Pre Header -->
     <div id="pre-header">
       <div class="container">
         <div class="row">
           <div class="col-md-12">
-            <span>Good things in life come to those who adopt animals!</span>
+            <span>JJ will add a nice line here</span>
           </div>
         </div>
       </div>
@@ -55,12 +69,19 @@ if ($conn->connect_error) {
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-              <a class="nav-link" href="index.php">Return to Home</a>
+              <a class="nav-link" href="index.html">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="animals.php">Animals
+              <a class="nav-link" href="products.html">Products
                 <span class="sr-only">(current)</span>
               </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.html">About Us</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="contact.html">Contact Us</a>
+              <span class="sr-only">(current)</span>
             </li>
           </ul>
         </div>
@@ -78,7 +99,7 @@ if ($conn->connect_error) {
               <h1>Add new animal</h1>
             </div>
           </div>
-
+          
           </div>
           <div class="col-md-6">
             <div class="right-content">
@@ -121,7 +142,7 @@ if ($conn->connect_error) {
                         <input name="Description" type="text" class="form-control" id="Description" placeholder="Describe em!" required="">
                       </fieldset>
                     </div>
-
+                  			
                     <!--
                     <div class="col-md-12">
                       <fieldset>
@@ -134,6 +155,9 @@ if ($conn->connect_error) {
                       </fieldset>
                     </div>
                     <div class="col-md-12">
+                      <div class="share">
+                        <h6>You can also keep in touch on: <span><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-linkedin"></i></a><a href="#"><i class="fa fa-twitter"></i></a></span></h6>
+                      </div>
                     </div>
                   </div>
                 </form>
@@ -151,12 +175,16 @@ if ($conn->connect_error) {
 <?php $Breed = $_POST['Breed']; ?>
 <?php $Color = $_POST['Color']; ?>
 <?php $Description = $_POST['Description']; ?>
-<?php $d=strtotime("today");
+<?php $d=strtotime("today"); 
 $Posted_date = date("Y-m-d h:i:s", $d); ?>
 
 <?php $sql = "INSERT INTO animal(Animal_type, Name, Age, Size, Breed, Color, Posted_date, Description, Is_available, Num_of_likes) VALUES ('$Animal_type','$Name','$Age','$Size','$Breed','$Color','$Posted_date','$Description','1','0');";
 
-  $result = $conn->query($sql);
+if ($conn->query($sql) === TRUE) {
+  header('Location: animals.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 if ($Animal_type=="Horse") {
     $sql = "INSERT INTO Horse VALUES (@@IDENTITY,0);";
@@ -164,8 +192,46 @@ if ($Animal_type=="Horse") {
     $sql = "INSERT INTO Snake VALUES (@@IDENTITY,0);";
 }
   $result = $conn->query($sql);
+
+  
   mysqli_query($result);
 ?>
+
+        <!-- Footer Starts Here -->
+        <div class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="logo">
+              <img src="assets/images/header-logo.png" alt="">
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="footer-menu">
+              <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">How It Works ?</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="social-icons">
+              <ul>
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#"><i class="fa fa-rss"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Footer Ends Here -->
+
 
     <!-- Sub Footer Starts Here -->
     <div class="sub-footer">
@@ -173,7 +239,8 @@ if ($Animal_type=="Horse") {
         <div class="row">
           <div class="col-md-12">
             <div class="copyright-text">
-              <p>Copyright &copy; 2019 Jamshed Jahangir</p>
+              <p>Copyright &copy; 2019 ShelterYou 
+               <a rel="nofollow" href="https://www.facebook.com/tooplate">Tooplate</a></p>
             </div>
           </div>
         </div>
